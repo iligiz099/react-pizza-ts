bd4cba09b2cfc9497d866062a5a486b560ec170d
Iligiz <polimpolim2000@gmail.com>
2022-07-18 16:37:47 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
+import {  Routes, Route } from "react-router-dom";
 
 import Header from './components/Header';
-import Categories from './components/Categories';
-import Sort from './components/Sort';
-import PizzaBlock from './components/PizzaBlock';
-
-import pizzas from './assets/pizzas.json'
+import Home from './pages/Home'
+import Cart from './pages/Cart'
+import NotFound from "./pages/NotFound";
 
 import './scss/app.scss';
 
@@ -17,16 +16,11 @@ function App() {
   <Header />
     <div className="content">
       <div className="container">
-        <div className="content__top">
-         <Categories />
-          <Sort />
-        </div>
-        <h2 className="content__title">Все пиццы</h2>
-        <div className="content__items">
-            {pizzas.map((obj) => (
-              <PizzaBlock key={obj.id} {...obj} />
-            ))}
-        </div>
+       <Routes>
+         <Route path="/" element={<Home />} />
+         <Route path="/cart" element={<Cart />} />
+         <Route path="*" element={<NotFound />} />
+       </Routes>
       </div>
     </div>
   </div>
---
+++ src/components/NotFoundBlock/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import styles from './NotFoundBlock.module.scss'
+
+export const NotFoundBlock = () => {
+    return (
+        <div className={styles.root}>
+            <h1>Not Found :(</h1>
+        </div>        
+        
+    );
+};
+
+
+export default NotFoundBlock;
